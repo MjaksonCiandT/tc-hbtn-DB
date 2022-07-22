438710cfb81855944eddb9ad70f6a1a51f7eb1ac
Maikon Jakson <mjakson@ciandt.com>
2022-07-22 10:56:03 -0300

    Java com Banco de Dados - Introdução _ SUBINDO TODAS AS TASKs

---
+++ 0x00/salvando/sql_schema_first_db.sql
@@ -0,0 +1,10 @@
+-- TABLE
+CREATE TABLE CLIENTE(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nome varchar(80) NOT NULL, email varchar (40) NOT NULL, telefone integer);
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
